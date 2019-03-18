f5ce1bb52ef58598f4bac381898b95205018b445
Edgar Fernandez <edgarfeav@hotmail.com>
2019-03-18 16:23:28 +0100

    añadido una area

---
+++ Portafolios/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model;
+
+namespace Portafolios.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private TablaDato tabladato = new TablaDato();
+        // GET: Default
+        public string Index()
+        {
+            return "Hola";
+        }
+    }
+}
